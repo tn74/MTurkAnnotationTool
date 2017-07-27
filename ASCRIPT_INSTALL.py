6d45de91308318067e3b4a4fb5ed7a0a26ab13e7
tn74 <ntrishul24@gmail.com>
2017-07-27 09:06:59 -0400

    Deleted config

--- ASCRIPT_INSTALL.py
+++ ASCRIPT_INSTALL.py
@@ -1,8 +1,19 @@
 from subprocess import call
 import subprocess
 import json
+import configparser
 
-firebaseProjectID = 'Your Project ID HERE'
+
+firebaseProjectID = 'amtannotate3'
+
+config = configparser.RawConfigParser()
+config.add_section('Username Here')
+config.add_section('SetUp')
+config.set('SetUp', 'firebaseSubdomain', firebaseProjectID)
+config.set('Username Here', 'awskey', 'Your AWS Key Here')
+config.set('Username Here', 'awssakey', 'Your AWS Secret Access Key')
+with open('config.ini', 'w') as configfile:
+    config.write(configfile)
 todump = {}
 todump["projects"] = {}
 todump["projects"]["default"] = firebaseProjectID
@@ -11,6 +22,6 @@ def subprocess_cmd(command):
     process = subprocess.Popen(command,stdout=subprocess.PIPE, shell=True)
     proc_stdout = process.communicate()[0].strip()
     print (proc_stdout)
-    proc_stdout = process.communicate()[0].strip()
 print('Deploying Site... This may take a few minutes')
-subprocess_cmd ('cd toWeb && firebase deploy')
+subprocess_cmd ('cd toWeb && firebase deploy ')
+print('Site Deployed! Installation Finished. Please Enter your configuration keys inside the config.ini file')
