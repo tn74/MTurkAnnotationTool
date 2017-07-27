27ef332dd9e208d9105de391530d9537ba962a4f
tn74 <ntrishul24@gmail.com>
2017-07-27 07:42:26 -0400

    Updated Installation Script

--- ASCRIPT_INSTALL.py
+++ ASCRIPT_INSTALL.py
@@ -1,7 +1,16 @@
+from subprocess import call
+import subprocess
+import json
+
+firebaseProjectID = 'Your Project ID HERE'
+todump = {}
+todump["projects"] = {}
+todump["projects"]["default"] = firebaseProjectID
+json.dump(todump, open('toWeb/.firebaserc', 'w'))
 def subprocess_cmd(command):
     process = subprocess.Popen(command,stdout=subprocess.PIPE, shell=True)
     proc_stdout = process.communicate()[0].strip()
     print (proc_stdout)
+    proc_stdout = process.communicate()[0].strip()
 print('Deploying Site... This may take a few minutes')
-subprocess_cmd ('cd toWeb')
-subprocess_cmd ('firebase init hosting') # Puts toWeb folder online at firebase
+subprocess_cmd ('cd toWeb && firebase deploy')
