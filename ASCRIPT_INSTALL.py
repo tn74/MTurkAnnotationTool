fb68d099cfae96649fefb8f1a4e654e3198922c2
tn74 <ntrishul24@gmail.com>
2017-07-28 00:20:06 -0400

    Removed merge folders

--- ASCRIPT_INSTALL.py
+++ ASCRIPT_INSTALL.py
@@ -1,10 +1,39 @@
+"""
+Author: Trishul Nagenalli (tn74@duke.edu)
+Organization: Duke Energy Data Analytics Lab
+
+Description:
+Use this script to help speed the installation process on your computer. The primary purpose of this program is to get 
+your firebase site up and running. To do so, you must have installed the Firebase CLI and have created a firebase 
+project on https://console.firebase.google.com/
+
+If you have not already done so, please open your terminal, move into this folder and run:
+firebase login
+and login to the google account associated with your firebase project
+
+You must know your firebase project's ID to run this install script. Enter
+firebase list
+into the terminal to see all projects associated with your firebase account and their respective IDs. Set 
+the variables firebaseProjectID to be the ID you wish to use
+
+Once this script has completed, you will need to add your AWS Access and Secret Access Keys into the config file
+
+Variable Information
+
+firbaseProjectID				- 	The id of your firebase project that will be used to commit host your the 
+									annnotation tool online
+"""
+#====================== Set Variables below
+firebaseProjectID = 'Your firebase project id here'
+#====================== 
+
+
+
 from subprocess import call
 import subprocess
 import json
 import configparser
 import os
-
-firebaseProjectID = 'Your firebase project id here'
 
 if not (os.path.exists('config.ini')):
 	config = configparser.RawConfigParser()
