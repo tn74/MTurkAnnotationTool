8806ada214271eba34687aba9d762632fa8f2fb5
tn74 <ntrishul24@gmail.com>
2017-10-02 14:19:35 -0400

    Added images for bottom and right borders

--- imCut.py
+++ imCut.py
@@ -2,17 +2,17 @@ from PIL import Image
 import os
 
 ## INPUT ITEMS
-def cut(inFile):
+def cut(inFile, length, overlap):
 	"""
 	:param inFile: name of image file to cut up for posting on mturk. Image must be inside folder imToCut
+	:param length: length of a side of a square for an individual image to be cut up
+	:param overlap: overlap between images
 	:return: no return. Stores all cut images in toWeb/public/images/[image name without extension]
 	"""
+
 	upper = 0
 	left = 0 
 	count = 0
-	length  = 400
-	overlap = 100
-
 	imagename = 'imToCut/'+inFile	 # INSERT THE IMAGE NAME THAT YOU WANT TO CUT HERE
 	direc = imagename.split('/')
 	iname = direc[len(direc)-1].split('.')[0]
@@ -24,20 +24,53 @@ def cut(inFile):
 	width, height = im.size
 	imagesize = width
 	 
-	while upper < height:
-		while left < width:
+	while upper < height-length:
+		while left < width-length:
 			bbox = (left, upper, left + length, upper + length)
 			working_slice = im.crop(bbox)
 			tempw, temph = working_slice.size
-			#print ("Temp: " + str(tempw) + " " + str(temph))
-			if (left+length)<=imagesize and (upper+length)<=imagesize:	
-				outfilename = os.path.join(outdir, iname + "_" + "U" + str(upper).zfill(4) + "_L" + str(left).zfill(4) + ".jpg")
-				working_slice.save(outfilename)
+			outfilename = os.path.join(outdir, iname + "_" + "U" + str(upper).zfill(4) + "_L" + str(left).zfill(4) + ".jpg")
+			working_slice.save(outfilename)
 			count +=1
 			left += length-overlap
+			print("Upper Dimension: " + str(upper) + ", Left Dimension: " + str(left))
 		upper += length-overlap
 		left = 0
-		print("Upper Dimension: " + str(upper))
+		
+	# Right Border Images
+	upper = 0
+	left = width - length
+	while upper < height-length:
+		bbox = (left, upper, left + length, upper + length)
+		working_slice = im.crop(bbox)
+		outfilename = os.path.join(outdir, iname + "_" + "U" + str(upper).zfill(4) + "_L" + str(left).zfill(4) + ".jpg")
+		working_slice.save(outfilename)
+		print("Upper Dimension: " + str(upper) + ", Left Dimension: " + str(left))
+		upper += length-overlap
+
+
+	
+	# Bottom Border Images
+	upper = height - length
+	left = 0
+	while left < width-length:
+		bbox = (left, upper, left + length, upper + length)
+		working_slice = im.crop(bbox)
+		outfilename = os.path.join(outdir, iname + "_" + "U" + str(upper).zfill(4) + "_L" + str(left).zfill(4) + ".jpg")
+		working_slice.save(outfilename)
+		print("Upper Dimension: " + str(upper) + ", Left Dimension: " + str(left))
+		left += length-overlap
+
+
+	# Bottom Right Image
+	left = width-length
+	upper = height - length
+	bbox = (left, upper, left + length, upper + length)
+	working_slice = im.crop(bbox)
+	outfilename = os.path.join(outdir, iname + "_" + "U" + str(upper).zfill(4) + "_L" + str(left).zfill(4) + ".jpg")
+	working_slice.save(outfilename)
+	print("Upper Dimension: " + str(upper) + ", Left Dimension: " + str(left))
+
 
 	imagename = imagename[:str.rindex(imagename,'.')]
 	savejpg = imagename + '.jpg'
